e56e1d461b9f02c672bf6a45213bae683a0a1e41
Nelson <nelson070585@gmail.com>
2024-01-08 00:48:29 -0300

    desarrollo de la estrella (renderización condicional con una función toggle) ademas de su estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
 import { LandingPages } from './components/LandingPages/LandingPages';
 
 function App() {
--- src/components/Card/CoffeCard.jsx
+++ src/components/Card/CoffeCard.jsx
@@ -1,6 +1,20 @@
 import '../Card/CoffeCard.css';
+import { useState } from 'react';
+
 
 export const CoffeCard = ({coffe}) => {
+
+    // const votos = coffe.votes
+    // const [ votes, setVotes ] = useState(votos)
+    const [ star, setStar ] = useState(false)
+    const starLine = <img src="../../src/assets/Star.svg" className='coffeStar' alt="star" />;
+    const starFill = <img src="../../src/assets/Star_fill.svg" className='coffeStar_fill' alt="star fill" />;
+
+
+
+    const toggleStar = () => {
+        setStar((starL) => !starL);
+    }
 
     return(
         <div className='coffeCard'>
@@ -9,7 +23,14 @@ export const CoffeCard = ({coffe}) => {
                 <h5 className='coffeCard__name'>{coffe.name}</h5>
                 <p className='coffeCard__price'>{coffe.price}</p>
             </section>
-
+            <section className='coffeRating' >
+                <span onClick={toggleStar} className='coffeStar'>{
+                    !star ? starLine : starFill
+                }</span>
+                <p>{coffe.rating}</p>
+                <p>({coffe.votes} votes)</p>
+            
+            </section>
         </div>
     )
 }
