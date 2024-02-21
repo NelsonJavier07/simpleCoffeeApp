2df27059e07209538957b11c47c1f531dc0bf600
Nelson <nelson070585@gmail.com>
2024-02-21 18:29:51 -0300

    1ra Corrección en rutas para corregir el despliegue

--- src/components/Card/CoffeCard.jsx
+++ src/components/Card/CoffeCard.jsx
@@ -5,9 +5,10 @@ import { useState } from 'react';
 export const CoffeCard = ({coffe}) => {
 
     const [ star, setStar ] = useState(false)
-    const starLine = <img src="public/images/star.svg" className='coffeStar' alt="star" />;
-    const starFill = <img src="public/images/star_fill.svg" className='coffeStar_fill' alt="star fill" />;
+    const starLine = <img src="images/star.svg" className='coffeStar' alt="star" />;
+    const starFill = <img src="images/star_fill.svg" className='coffeStar_fill' alt="star fill" />;
 
+    const {image, name, price, rating, votes} = coffe
 
 
     const toggleStar = () => {
@@ -16,17 +17,17 @@ export const CoffeCard = ({coffe}) => {
 
     return(
         <div className='coffeCard'>
-            <img src={coffe.image} className='coffeCard__img' alt={coffe.name} />
+            <img src={image} className='coffeCard__img' alt={name} />
             <section className='coffeCard__info'>
-                <h5 className='coffeCard__name'>{coffe.name}</h5>
-                <p className='coffeCard__price'>{coffe.price}</p>
+                <h5 className='coffeCard__name'>{name}</h5>
+                <p className='coffeCard__price'>{price}</p>
             </section>
             <section className='coffeRating' >
                 <span onClick={toggleStar} className='coffeStar'>{
                     !star ? starLine : starFill
                 }</span>
-                <p>{coffe.rating}</p>
-                <p>({coffe.votes} votes)</p>
+                <p>{rating}</p>
+                <p>({votes} votes)</p>
             
             </section>
         </div>
